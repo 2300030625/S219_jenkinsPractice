e13dac804608aec315b47acfdf6644ae6c356ceb
2300030625 <163733030+2300030625@users.noreply.github.com>
2025-08-05 11:18:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecomJenkins-frontend">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
